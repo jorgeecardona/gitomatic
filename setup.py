62f56c4572783a6eed4a238059ec32c2f0eb0a16
Jorge Eduardo Cardona <jorgeecardona@gmail.com>
2010-12-31 22:55:51 -0500

    Add __all__ to gitomatic/__init__.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gitomatic',
-    version='0.3.8',
+    version='0.3.9',
     description='Git management tool',
     author='Jorge Eduardo Cardona',
     author_email='jorge.cardona@nuagehq.com',
